1b266db3c0c37da2996cd0abba62d216dd7bf29c
Shirp-l <exentrify@gmail.com>
2022-08-31 17:43:14 -0500

    fixed create edit & completion utils

--- src/utils/createCompletion.ts
+++ src/utils/createCompletion.ts
@@ -1,28 +1,58 @@
-import { getConfig } from '../config';
 import { Configuration, OpenAIApi } from "openai";
-
-
-
+import {getConfig} from "../config";
 
 const config = getConfig();
 
 const configuration = new Configuration({
-  apiKey: config.openAiApiKey ? process.env.OPENAIAI_API_KEY : '',
+    apiKey: config.openAiApiKey ? process.env.OPENAIAI_API_KEY : '',
 });
 const openai = new OpenAIApi(configuration);
 
 
-export const getCompletionResult = async (req: string, res: { status: (statusCode: number) => { (): any; new(): any; json: { (resText: { text: string | undefined; }): void; new(): any; }; }; }) => {
-	const createCompletionRequest = await openai.createCompletion({
-    /* eslint-disable @typescript-eslint/naming-convention */
-    model: config.completionsModel,
-    prompt: req,
-    temperature: config.temperature,
-    max_tokens: config.maxTokens, 
-    top_p: config.topP,
-    presence_penalty: config.presencePenalty,
-    frequency_penalty: config.frequencyPenalty,
-    stop: config.completionEngineDefaultstop.length === 0 ? null : config.completionEngineDefaultstop // API does not accept empty array
-  });
-  res.status(200).json({text:  createCompletionRequest.data.choices[0].text });
+
+export type CreateCompletionResult = {
+    createCompletionResultChoices: string,
+};
+
+
+/**
+ * Send a request to the OpenAI server to get the tokens following prompt.
+ */
+export const  getCompletionResult = (req: string): Promise<CreateCompletionResult> => {
+    return new Promise(async (resolve, reject) => {
+        await openai.createCompletion({
+            /* eslint-disable @typescript-eslint/naming-convention */
+            model: config.completionsModel,
+            prompt: req,
+            max_tokens: config.maxTokens,
+            temperature: config.temperature,
+            top_p: config.topP,
+            n: config.n,
+            stop: config.completionEngineDefaultstop,
+            presence_penalty: config.presencePenalty,
+            frequency_penalty: config.frequencyPenalty
+        })
+            .then((res: any) => {
+                const resp = JSON.parse(res.text);
+
+                if (resp["choices"].length === 0) {
+                    reject(new Error("No code returned by the server."));
+                } else {
+                    resolve((resp["choices"][0]["text"]));
+                }
+            })
+            .catch((Error: any) => {
+                if (Error) {
+                    let message: string;
+                    // If the API specified an error message, return it.
+                    if (Error.response.text) {
+                        message = JSON.parse(Error.response.text).error.message;
+                    } else {
+                        message = Error;
+                    }
+                    reject(new Error(message));
+                }}
+
+            );
+    });
 };
--- src/utils/createEdit.ts
+++ src/utils/createEdit.ts
@@ -1,23 +1,55 @@
-import { getConfig } from '../config';
 import { Configuration, OpenAIApi } from "openai";
+import {getConfig} from "../config";
 
 const config = getConfig();
 
 const configuration = new Configuration({
-  apiKey: config.openAiApiKey ? process.env.OPENAIAI_API_KEY : '',
+    apiKey: config.openAiApiKey ? process.env.OPENAIAI_API_KEY : '',
 });
 const openai = new OpenAIApi(configuration);
 
 
-export const getEditResult = async (req: string) => {
-	const createEditRequest = await openai.createEdit({
-    /* eslint-disable @typescript-eslint/naming-convention */
-    model: config.editsModel,
-    input: req,
-    instruction: config.editIntructions,
-    n: config.n,
-    temperature: config.temperature,
-    top_p: config.topP,
-  });
-  const editResult = createEditRequest.data.choices[0].text;
+
+export type CreateEditResult = {
+    createEditResultChoices: string,
+};
+
+
+/**
+ * Send a request to the OpenAI server to get the tokens following prompt.
+ */
+export const  getEditResult = (req: string): Promise<CreateEditResult> => {
+    return new Promise(async (resolve, reject) => {
+        await openai.createEdit({
+            /* eslint-disable @typescript-eslint/naming-convention */
+            model: config.editsModel,
+            input: req,
+            instruction: config.editInstructions,
+            n: config.n,
+            temperature: config.temperature,
+            top_p: config.topP
+        })
+            .then((res: any) => {
+                const resp = JSON.parse(res.text);
+
+                if (resp["choices"].length === 0) {
+                    reject(new Error("No code returned by the server."));
+                } else {
+                    resolve((resp["choices"][0]["text"]));
+                }
+            })
+            .catch((Error: any) => {
+                if (Error) {
+                    let message: string;
+                    // If the API specified an error message, return it.
+                    if (Error.response.text) {
+                        message = JSON.parse(Error.response.text).error.message;
+                    } else {
+                        message = Error;
+                    }
+                    reject(new Error(message));
+                }}
+
+            );
+    });
 };
